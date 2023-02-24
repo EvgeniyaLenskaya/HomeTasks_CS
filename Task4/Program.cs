4d0f732f3e3379120d9c84b830ae51ff59dc9ed0
EvgeniyaLenskaya <janie2010@mail.ru>
2023-02-24 19:12:44 +0800

    add negative

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,8 +1,11 @@
 ﻿Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+if (number < 0)
+{
+    number = number * (-1);
+}
 int i = 1;
-
 while (i <= number)
 {
     if (i % 2 == 0)
