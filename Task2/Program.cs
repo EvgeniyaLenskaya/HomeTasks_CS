ee24d2a3e6e03a22cd13b43b7e1a00b42da3b349
EvgeniyaLenskaya <janie2010@mail.ru>
2023-02-24 17:34:24 +0800

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("Максимальное чиcло - ");
+Console.WriteLine (max);
