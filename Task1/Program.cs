7e7e720951aea19198e0672b187ea08abd724301
EvgeniyaLenskaya <janie2010@mail.ru>
2023-02-24 16:45:39 +0800

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB>numberA)
+{
+    Console.WriteLine(numberB + " - максимальное число");
+}
+
+else
+{
+    Console.WriteLine(numberA + " - максимальное число");
+}
